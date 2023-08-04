80924c66563d1ea71979d85d26fb8267bbfa41a9
jmccallister93 <jaredmccallister93@gmail.com>
2023-08-04 13:20:15 -0700

    working on stat extract still

--- prep-pal/src/pages/HomePage.js
+++ prep-pal/src/pages/HomePage.js
@@ -5,28 +5,102 @@ const HomePage = (props) => {
   const [text, setText] = useState("");
 
   function parseCharacterData(text) {
-    // Split the text into lines
     const lines = text.split("\n");
-
-    // Initialize an empty object to hold the character data
     const character = {};
+    let section = null;
+    // Define a list of possible class names
+    const classes = [
+      "Wizard",
+      "Druid",
+      "Ranger",
+      "Paladin",
+      "Warlock",
+      "Sorcerer",
+      "Cleric",
+      "Bard",
+      "Fighter",
+      "Monk",
+      "Rogue",
+      "Barbarian",
+    ];
 
-    // Iterate over the lines
     for (let i = 0; i < lines.length; i++) {
-      // Trim the line to remove leading/trailing whitespace
       const line = lines[i].trim();
 
-      // Check if the line matches specific patterns and extract data accordingly
-      if (line === "TopShelf13") {
-        // The character's name is on the next line
-        character.name = lines[i + 1].trim();
-      } else if (line === "Ability Scores") {
-        // The ability scores start on the next line and continue for six lines
+      // Skip the "Search Everything..." line and empty lines
+      if (line === "Search Everything..." || line === "") {
+        continue;
+      }
+
+      // Extract player and character names
+      if (!character.playerName) {
+        character.playerName = line;
+        character.characterName = lines[i + 1].trim();
+        i++; // Skip the next line
+        continue;
+      }
+
+      // Skip the "Manage" line and empty lines
+      if (line === "MANAGE" || line === "") {
+        continue;
+      }
+
+      // Extract class and level
+      if (!character.class) {
+        for (let j = 0; j < classes.length; j++) {
+          const classIndex = line.indexOf(classes[j]);
+          if (classIndex !== -1) {
+            character.class = classes[j];
+            character.level = Number(
+              line.slice(classIndex + classes[j].length).trim()
+            );
+            break;
+          }
+        }
+        continue;
+      }
+
+      // Check for section headers
+      if (line === "Ability Scores") {
+        section = "Ability Scores";
         character.abilities = {};
-        for (let j = 0; j < 6; j++) {
-          const abilityLine = lines[i + j + 1].trim();
-          const [ability, score] = abilityLine.split("\n").map((x) => x.trim());
-          character.abilities[ability] = Number(score);
+      } else if (line === "Character Health") {
+        section = "Character Health";
+        character.health = {};
+      } else if (line === "Proficiencies and Languages") {
+        section = "Proficiencies and Languages";
+        character.proficiencies = {};
+      } else if (section === "Ability Scores") {
+        // Parse ability scores
+        [
+          "STRENGTH",
+          "DEXTERITY",
+          "CONSTITUTION",
+          "INTELLIGENCE",
+          "WISDOM",
+          "CHARISMA",
+        ].forEach((ability) => {
+          if (line === ability) {
+            const modifierLine = lines[i + 3].trim(); // Skip one line and + symbol
+            const scoreLine = lines[i + 4].trim(); // Score is in the next line
+            const modifier = Number(modifierLine);
+            const score = Number(scoreLine);
+            character.abilities[ability] = { modifier, score };
+          }
+        });
+      } else if (section === "Character Health") {
+        // Parse health
+        if (line === "CURRENT") {
+          const healthLine = lines[i + 1].trim();
+          const [current, max] = healthLine.split(" / ").map((x) => Number(x));
+          character.health.current = current;
+          character.health.max = max;
+        }
+      } else if (section === "Proficiencies and Languages") {
+        // Parse proficiencies and languages
+        if (line === "LANGUAGES") {
+          const languagesLine = lines[i + 1].trim();
+          character.proficiencies.languages = languagesLine.split(", ");
         }
       }
     }
@@ -41,8 +115,6 @@ const HomePage = (props) => {
     console.log(character);
     // Here you can do something with the character data, like saving it to state or sending it to a server
   };
-
-  const character = parseCharacterData(text);
 
   return (
     <>
