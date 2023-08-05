00b96b6f066619f97044b24ed6d6ec750405c099
jmccallister93 <jaredmccallister93@gmail.com>
2023-08-04 19:49:21 -0700

    Got the api working with the character sheet

--- prep-pal/src/components/CharacterAPI.js
+++ prep-pal/src/components/CharacterAPI.js
@@ -6,51 +6,51 @@ const CharacterAPI = (props) => {
 
   useEffect(() => {
     fetch(
-      "http://localhost:5000/proxy/https://character-service.dndbeyond.com/character/v3/character/35293785"
+      `http://localhost:5000/proxy/https://character-service.dndbeyond.com/character/v3/character/${props.id}` 
     )
       .then((response) => response.json())
       .then(({ data }) => {
         setData(data);
         const newCharacter = {
           name: data.name,
-          baseHitPoints: data.baseHitPoints,
-          actions: data.actions,
-          strength: data.stats[0],
-          dextertiy: data.stats[1],
-          constitution: data.stats[2],
-          intelligence: data.stats[3],
-          wisdom: data.stats[4],
-          charisma: data.stats[5],
-          racialTraits: data.race.racialTraits, //data.race.racialTraits[0].definition.description
-          walkSpeed: data.race.weightSpeeds.normal.walk,
-          flySpeed: data.race.weightSpeeds.normal.fly,
-          burrowSpeed: data.race.weightSpeeds.normal.burrow,
-          swimSpeed: data.race.weightSpeeds.normal.swim,
-          climbSpeed: data.race.weightSpeeds.normal.climb,
-        classLevel: data.classes.level,
-        class: data.classes.definition.name,
-        spells: data.spells.class,
-        racialActions: data.actions.race,
-        classActions: data.actions.class,
-        modifiers: data.modifiers,
-        classSpells: data.classSpells.spells,
+          // baseHitPoints: data.baseHitPoints,
+          // actions: data.actions,
+          // strength: data.stats[0],
+          // dextertiy: data.stats[1],
+          // constitution: data.stats[2],
+          // intelligence: data.stats[3],
+          // wisdom: data.stats[4],
+          // charisma: data.stats[5],
+          // racialTraits: data.race.racialTraits, //data.race.racialTraits[0].definition.description
+          // walkSpeed: data.race.weightSpeeds.normal.walk,
+          // flySpeed: data.race.weightSpeeds.normal.fly,
+          // burrowSpeed: data.race.weightSpeeds.normal.burrow,
+          // swimSpeed: data.race.weightSpeeds.normal.swim,
+          // climbSpeed: data.race.weightSpeeds.normal.climb,
+          // classLevel: data.classes.level,
+          // class: data.classes.definition.name,
+          // spells: data.spells.class,
+          // racialActions: data.actions.race,
+          // classActions: data.actions.class,
+          // modifiers: data.modifiers,
+          // classSpells: data.classSpells.spells,
         };
         setCharacter(newCharacter);
       })
       .catch((error) => console.error(error));
-  }, []);
-//   console.log(data.race.racialTraits[0].definition.description)
+  }, [props.id]);
+  //   console.log(data.race.racialTraits[0].definition.description)
 
   return (
     <div className="App">
       {/* Check if character has been set before trying to access its name property */}
-      {data && <pre style={{}}>{JSON.stringify(data, null, 2)}</pre>}
+      {/* {data && <pre style={{}}>{JSON.stringify(data, null, 2)}</pre>} */}
       {character && (
         <>
-          <h1>{character.name}</h1> 
-          <h2>{character.baseHitPoints}</h2>
+          <h1>{character.name}</h1>
+          {/* <h2>{character.baseHitPoints}</h2>
           <h2>{character.actions.class[0].actionType}</h2>
-          <h2>{character.walkSpeed}</h2>
+          <h2>{character.walkSpeed}</h2> */}
         </>
       )}
     </div>
--- prep-pal/src/pages/HomePage.js
+++ prep-pal/src/pages/HomePage.js
@@ -3,11 +3,25 @@ import GetCharSheet from "../components/GetCharSheet";
 import CharacterAPI from "../components/CharacterAPI";
 
 const HomePage = (props) => {
+  const [id, setId] = useState('')
+
+  const handleInputChange = (event) => {
+    setId(event.target.value);
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // You can perform other actions on form submit here, if needed
+  }
 
   return (
     <>
       <div>
-        <CharacterAPI />
+      <form onSubmit={handleSubmit}>
+          <input type="text" value={id} onChange={handleInputChange} />
+          <button type="submit">Submit</button>
+        </form>
+        <CharacterAPI id={id}/>
       </div>
     </>
   );
